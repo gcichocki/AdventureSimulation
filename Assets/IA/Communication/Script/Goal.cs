c0e75fd9789c5b866c3d43646c530ad7db2d63cd
gcichocki <guilhem.cichocki@gmail.com>
2018-06-08 17:32:04 -0400

    goals

--- Assets/IA/Communication/Script/Goal.cs
+++ Assets/IA/Communication/Script/Goal.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goal  {
+public class Goal : IComparable<Goal>{
 
     public enum Objective_T { RELIC, KEY, TRAP, MONSTER };
 
@@ -13,12 +14,41 @@ public class Goal  {
     [SerializeField] Transform location;
     [SerializeField] BaseEntity.Class_T concern;
 
-    public Goal(int id, Objective_T type, Transform location, BaseEntity.Class_T concern)
+    public Goal(Objective_T type, Transform location, BaseEntity.Class_T concern)
     {
         this.id = global_objective_id;
         this.type = type;
         this.location = location;
         this.concern = concern;
         global_objective_id++;
+    }
+
+    public int Score(Goal goal, BaseEntity.Class_T my_class)
+    {
+        if (goal.concern == my_class)
+        {
+            return 0;
+        }
+        if (goal.concern == BaseEntity.Class_T.ALL)
+        {
+            return 1;
+        }
+        return 2;
+    }
+
+    public int CompareTo(Goal other)
+    {
+        return CompareTo(other, BaseEntity.Class_T.FIGHTER);
+    }
+
+    public int CompareTo(Goal other, BaseEntity.Class_T my_class)
+    {
+        return Score(this, my_class) - Score(other, my_class);
+    }
+
+    public override string ToString()
+    {
+        return id.ToString();
+        //return id.ToString() + " " + type.ToString() + " " + location.position.ToString() + " " + concern.ToString();
     }
 }
---
+++ Assets/IA/Communication/Script/GoalQueue.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalQueue {
+
+    List<Goal> queue;
+    bool once = true;
+    public List<Goal> Queue { get { return queue; } set { queue = value; } }
+
+    public GoalQueue()
+    {
+        this.queue = new List<Goal>();
+    }
+
+    public void SortByPriority()
+    {
+        queue.Sort();
+    }
+
+   /* void Start()
+    {
+        this.queue = new List<Goal>();
+        Goal c = new Goal(Goal.Objective_T.TRAP, null, BaseEntity.Class_T.HUNTER);
+        queue.Add(c);
+        Goal g = new Goal(Goal.Objective_T.TRAP, null, BaseEntity.Class_T.FIGHTER);
+        queue.Add(g);
+        Goal a = new Goal(Goal.Objective_T.RELIC, null, BaseEntity.Class_T.ALL);
+        queue.Add(a);
+        Goal b = new Goal(Goal.Objective_T.KEY, null, BaseEntity.Class_T.ALL);
+        queue.Add(b);
+        Goal d = new Goal(Goal.Objective_T.TRAP, null, BaseEntity.Class_T.HEALER);
+        queue.Add(d);
+    }
+
+    void Update()
+    {
+        SortByPriority();
+        if (once)
+        {
+            foreach (Goal g in queue)
+            {
+                Debug.Log(g);
+            }
+            once = false;
+        }
+        
+        
+    }*/
+
+}
