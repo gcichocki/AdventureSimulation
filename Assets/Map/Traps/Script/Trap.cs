c1e7a81a85822ecdf13617f98c43973c33c7439a
gcichocki <guilhem.cichocki@gmail.com>
2018-06-09 15:19:51 -0400

    ID

---
+++ Assets/Map/Traps/Script/Trap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+    [SerializeField] int id;
+    [SerializeField] BaseEntity.Class_T concern;
+
+    public BaseEntity.Class_T Concern { get { return concern; } set { concern = value; } }
+    public int ID { get { return id; } set { id = value; } }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
