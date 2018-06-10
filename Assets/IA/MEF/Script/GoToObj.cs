e2de7c2b7a3b5a9129ec4a3a345d9f74ebbde787
gcichocki <guilhem.cichocki@gmail.com>
2018-06-10 11:54:20 -0400

    Fix

--- Assets/IA/MEF/Script/GoToObj.cs
+++ Assets/IA/MEF/Script/GoToObj.cs
@@ -17,25 +17,26 @@ public class GoToObj : State {
 
 	override
 	public void Execute(){
-        if (owner.GetComponent<Agent>().Vision.SenseAny())
+        Agent own = owner.GetComponent<Agent>();
+        if (own.Vision.SenseAny())
         {
-            HashSet<Transform> objects_in_view = owner.GetComponent<Agent>().Vision.SensedObjects;
+            HashSet<Transform> objects_in_view = own.Vision.SensedObjects;
             foreach (Transform t in objects_in_view)
             {
                 if (t.tag.Equals("TRAP"))
                 { // check ID et classe necessaire pour le trap
-                    if (owner.GetComponent<Agent>().Objectives.ContainsGoal(t.transform.GetComponent<Trap>().ID))
+                    if (own.Objectives.ContainsGoal(t.transform.GetComponent<Trap>().ID))
                     {
-                        Goal g = new Goal(Goal.Objective_T.TRAP, t, BaseEntity.Class_T.ALL);
-                        owner.GetComponent<Agent>().New_Objectives.AddGoal(g);
-                        owner.GetComponent<Agent>().Objectives.AddGoal(g);
+                        Goal g = new Goal(Goal.Objective_T.TRAP, t, BaseEntity.Class_T.ALL, own);
+                        owner.GetComponent<Agent>().New_Objectives.AddGoal(g, own);
+                        owner.GetComponent<Agent>().Objectives.AddGoal(g, own);
                     }
                 }
 
 
             }
-            owner.GetComponent<Agent>().Objectives.SortByPriority();
-            owner.GetComponent<Agent>().StateMachine.ChangeState();
+            own.Objectives.SortByPriority();
+            own.StateMachine.ChangeState();
         }
 	 }
 
