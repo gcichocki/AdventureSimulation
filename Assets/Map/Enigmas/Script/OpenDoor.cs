11bb3d7c0df96a909d9895dec7eae7a2560b92f0
gcichocki <guilhem.cichocki@gmail.com>
2018-06-14 18:19:15 -0400

    One bug and go

---
+++ Assets/Map/Enigmas/Script/OpenDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    [SerializeField] Interactable key1;
+    [SerializeField] Interactable key2;
+    [SerializeField] Interactable key3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(key1 == null && key2 == null && key3 == null)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+	}
+}
